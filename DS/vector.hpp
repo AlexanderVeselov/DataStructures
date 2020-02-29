4377415e758689355ba0ba34e2469cd1671fe51c
Alexander Veselov <Alexander_Veselov@yahoo.com>
2020-03-01 00:42:51 +0300

    Add Vector and iterator for it

--- DS/vector.hpp
+++ DS/vector.hpp
@@ -2,13 +2,92 @@
 #define VECTOR_HPP
 
 #include <numeric>
+#include <cassert>
 
 namespace ds
 {
 template<class T>
+class Vector_Iterator
+{
+public:
+    Vector_Iterator(T* ptr)
+        : ptr_(ptr)
+    {
+    }
+
+    T& operator*()
+    {
+        return *ptr_;
+    }
+
+    T* operator->()
+    {
+        return ptr_;
+    }
+
+    Vector_Iterator operator-(std::size_t diff)
+    {
+        return Vector_Iterator(ptr_ - diff);
+    }
+
+    Vector_Iterator operator+(std::size_t diff)
+    {
+        return Vector_Iterator(ptr_ + diff);
+    }
+
+    Vector_Iterator& operator++()
+    {
+        ++ptr_;
+        return *this;
+    }
+
+    Vector_Iterator& operator--()
+    {
+        --ptr_;
+        return *this;
+    }
+
+    Vector_Iterator operator++(int)
+    {
+        Vector_Iterator tmp = *this;
+        ++ptr_;
+        return tmp;
+    }
+
+    Vector_Iterator operator--(int)
+    {
+        Vector_Iterator tmp = *this;
+        --ptr_;
+        return tmp;
+    }
+
+    bool operator==(Vector_Iterator const& other)
+    {
+        return ptr_ == other.ptr_;
+    }
+
+    bool operator!=(Vector_Iterator const& other)
+    {
+        return ptr_ != other.ptr_;
+    }
+
+private:
+    T* ptr_;
+
+};
+
+template<class T>
 class Vector
 {
 public:
+    using iterator = Vector_Iterator<T>;
+
+    Vector()
+        : size_(0)
+        , capacity_(0)
+    {
+    }
+
     Vector(std::size_t count, T const& value)
         : size_(count)
         , capacity_(size_ * 4 / 3)
@@ -33,6 +112,43 @@ public:
         }
     }
 
+    void push_back(T const& value)
+    {
+        if (capacity_ == size_)
+        {
+            Reallocate(capacity_ > 0 ? capacity_ * 4 / 3 : 4);
+        }
+
+        ptr_[size_++] = value;
+    }
+
+    void pop_back()
+    {
+        --size_;
+    }
+
+    T& front()
+    {
+        assert(size_ > 0);
+        return ptr_[0];
+    }
+
+    T& back()
+    {
+        assert(size_ > 0);
+        return ptr_[size_ - 1];
+    }
+
+    iterator begin()
+    {
+        return iterator(ptr_);
+    }
+
+    iterator end()
+    {
+        return iterator(ptr_ + size_);
+    }
+
     std::size_t size() const
     {
         return size_;
@@ -41,6 +157,11 @@ public:
     std::size_t capacity() const
     {
         return capacity_;
+    }
+
+    T* data()
+    {
+        return ptr_;
     }
 
     T& operator[](std::size_t index)
@@ -54,11 +175,27 @@ public:
     }
 
 private:
+    void Reallocate(std::size_t new_capacity)
+    {
+        assert(new_capacity > capacity_);
+        capacity_ = new_capacity;
+
+        T* old = ptr_;
+        // Allocate new memory
+        ptr_ = new T[capacity_];
+        // Copy from the old memory to the new allocated memory
+        memcpy(ptr_, old, sizeof(T) * size_);
+        // Delete old memory
+        delete[] old;
+
+    }
+
     T* ptr_ = nullptr;
     std::size_t size_ = 0;
     std::size_t capacity_ = 0;
 
 };
+
 }
 
 #endif
--- DSTest/dstest.cpp
+++ DSTest/dstest.cpp
@@ -4,16 +4,22 @@
 
 int main()
 {
-    ds::Vector<int> vector(34, 222);
-    std::cout << vector[32] << std::endl;
-    std::cout << vector.size() << std::endl;
-    std::cout << vector.capacity() << std::endl;
+    ds::Vector<int> vector;
+    for (int i = 0; i < 10; ++i)
+    {
+        vector.push_back(rand() % 1000);
+    }
 
-    std::vector<int> std_vector(333, 333);
-    std::cout << std_vector.size() << std::endl;
-    std::cout << std_vector.capacity() << std::endl;
-    std_vector.reserve(100);
-    std::cout << std_vector.capacity() << std::endl;
+    for (auto it = vector.begin(); it != vector.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    //std::vector<int> std_vector;
+    //std_vector.push_back(333222);
+    //std::vector<int>::iterator end = std_vector.end();
+    //std::vector<int>::iterator it = end - 1;
+    //std::cout << *it << std::endl;
 
     return 0;
 }
